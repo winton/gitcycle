85ed7dfc1d870e7c206ea11dfec50e14a939f4c5
winton <mail@wintoni.us>
2012-10-20 23:25:43 -0700

    Fixes for gitc pull and push with unknown branch

--- lib/gitcycle.rb
+++ lib/gitcycle.rb
@@ -271,14 +271,14 @@ class Gitcycle
     if branch && branch['collab']
       # Merge from collab
       merge_remote_branch(
-        :owner => branch['home'],
+        :owner => owner = branch['home'],
         :repo => branch['repo']['name'],
         :branch => branch['source']
       )
     elsif branch
       # Merge from upstream source branch
       merge_remote_branch(
-        :owner => branch['repo']['owner'],
+        :owner => owner = branch['repo']['owner'],
         :repo => branch['repo']['name'],
         :branch => branch['source']
       )
@@ -288,13 +288,13 @@ class Gitcycle
 
       # Merge from upstream branch with same name
       merge_remote_branch(
-        :owner => repo['owner'],
+        :owner => owner = repo['owner'],
         :repo => repo['name'],
         :branch => current_branch
       )
     end
 
-    unless branch && branch['collab']
+    unless branch && branch['collab'] || owner == @git_login
       # Merge from origin
       merge_remote_branch(
         :owner => @git_login,
@@ -313,7 +313,7 @@ class Gitcycle
 
     branch = pull
 
-    if branch['collab']
+    if branch && branch['collab']
       puts "\nPushing branch '#{branch['home']}/#{branch['name']}'.\n".green
       run("git push #{branch['home']} #{branch['name']} -q")
     elsif branch
