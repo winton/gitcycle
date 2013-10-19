52f291062251c2ca5ccc576161e04e3d48fc68ea
Winton Welsh <mail@wintoni.us>
2013-10-18 23:47:40 -0700

    Even shorter develop spec variables

--- spec/gitcycle/develop_spec.rb
+++ spec/gitcycle/develop_spec.rb
@@ -13,7 +13,7 @@ describe Gitcycle do
       "https://test.lighthouseapp.com/projects/0000/tickets/0000-ticket"
     end
 
-    let(:webmock_put_merge) do
+    let(:webmock_put) do
       { :request => { :home => "git_login" } }
     end
 
@@ -28,7 +28,7 @@ describe Gitcycle do
 
     context "with a lighthouse ticket" do
 
-      let(:webmock_post_merge) do
+      let(:webmock_post) do
         {
           :request  => { :lighthouse_url => lighthouse_url },
           :response => { :lighthouse_url => lighthouse_url }
@@ -36,8 +36,8 @@ describe Gitcycle do
       end
 
       before :each do
-        webmock(:branch, :post, webmock_post_merge)
-        webmock(:branch, :put,  webmock_put_merge)
+        webmock(:branch, :post, webmock_post)
+        webmock(:branch, :put,  webmock_put)
       end
 
       context "when the user accepts the default branch" do
@@ -61,8 +61,8 @@ describe Gitcycle do
         end
 
         it "requests and receives parameters that match the json spec" do
-          validate_schema(:branch, :post, webmock_post_merge)
-          validate_schema(:branch, :put,  webmock_put_merge)
+          validate_schema(:branch, :post, webmock_post)
+          validate_schema(:branch, :put,  webmock_put)
         end
 
         it "displays proper dialog", :capture do
@@ -106,8 +106,8 @@ describe Gitcycle do
 
       context "when the user changes the target branch" do
 
-        let(:webmock_put_merge_with_source) do
-          Gitcycle::Util.deep_merge(webmock_put_merge,
+        let(:webmock_put_with_source) do
+          Gitcycle::Util.deep(webmock_put,
             :request  => { :source => 'new-source' },
             :response => { :source => 'new-source' }
           )
@@ -115,7 +115,7 @@ describe Gitcycle do
 
         before :each do
           $stdin.stub(:gets).and_return("n", "new-source", "y")
-          webmock(:branch, :put, webmock_put_merge_with_source)
+          webmock(:branch, :put, webmock_put_with_source)
         end
 
         it "runs without assertions", :capture do
@@ -133,7 +133,7 @@ describe Gitcycle do
         end
 
         it "requests and receives parameters that match the json spec" do
-          validate_schema(:branch, :put, webmock_put_merge_with_source)
+          validate_schema(:branch, :put, webmock_put_with_source)
         end
 
         it "displays proper dialog", :capture do
@@ -149,7 +149,7 @@ describe Gitcycle do
 
     context "with a title" do
 
-      let(:webmock_post_merge) do
+      let(:webmock_post) do
         {
           :request  => { :title => 'new title' },
           :response => { :title => 'new title' }
@@ -159,8 +159,8 @@ describe Gitcycle do
       before :each do
         $stdin.stub(:gets).and_return("y")
         
-        webmock(:branch, :post, webmock_post_merge)
-        webmock(:branch, :put,  webmock_put_merge)
+        webmock(:branch, :post, webmock_post)
+        webmock(:branch, :put,  webmock_put)
       end
 
       it "runs without assertions", :capture do
@@ -178,8 +178,8 @@ describe Gitcycle do
       end
 
       it "requests and receives parameters that match the json spec" do
-        validate_schema(:branch, :post, webmock_post_merge)
-        validate_schema(:branch, :put,  webmock_put_merge)
+        validate_schema(:branch, :post, webmock_post)
+        validate_schema(:branch, :put,  webmock_put)
       end
 
       it "displays proper dialog", :capture do
@@ -194,7 +194,7 @@ describe Gitcycle do
     context "with a github issue" do
       let(:github_url) { 'https://github.com/login/repo/pull/0000' }
 
-      let(:webmock_post_merge) do
+      let(:webmock_post) do
         {
           :request  => { :github_url => github_url },
           :response => { :github_url => github_url }
@@ -204,8 +204,8 @@ describe Gitcycle do
       before :each do
         $stdin.stub(:gets).and_return("y")
         
-        webmock(:branch, :post, webmock_post_merge)
-        webmock(:branch, :put,  webmock_put_merge)
+        webmock(:branch, :post, webmock_post)
+        webmock(:branch, :put,  webmock_put)
       end
 
       it "runs without assertions", :capture do
@@ -223,8 +223,8 @@ describe Gitcycle do
       end
 
       it "requests and receives parameters that match the json spec" do
-        validate_schema(:branch, :post, webmock_post_merge)
-        validate_schema(:branch, :put,  webmock_put_merge)
+        validate_schema(:branch, :post, webmock_post)
+        validate_schema(:branch, :put,  webmock_put)
       end
 
       it "displays proper dialog", :capture do
