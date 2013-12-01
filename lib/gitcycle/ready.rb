e4019fed430bae91ddc979e335d52d9aa19ff0dc
Winton Welsh <mail@wintoni.us>
2013-12-01 15:33:36 -0800

    `ready` spec

--- lib/gitcycle/ready.rb
+++ lib/gitcycle/ready.rb
@@ -7,9 +7,20 @@ class Gitcycle < Thor
     branch = sync
     pr(true)
 
-    Git.checkout("qa-#{branch[:name]}", :branch => true)
+    qa_branch = "qa-#{branch[:name]}"
+
+    # Delete qa branch
+    Git.branch(qa_branch, :delete => true)
+    Git.push(":#{qa_branch}")
+
+    # Create new qa branch
+    Git.checkout(qa_branch, :branch => true)
+    
+    # Squash feature branch
     Git.merge_squash(branch[:name])
     Git.commit("##{branch[:github_issue_id]} #{branch[:title]}")
+
+    # Push qa branch & checkout feature branch
     Git.push("qa-#{branch[:name]}")
     Git.checkout(branch[:name])
   end
---
+++ spec/gitcycle/ready_spec.rb
@@ -0,0 +1,50 @@
+require File.expand_path("../../spec_helper", __FILE__)
+
+describe Gitcycle do
+
+  describe "#ready" do
+
+    let(:gitcycle) do
+      Gitcycle::Config.config_path = config_path
+      Gitcycle.new
+    end
+
+    before(:each) do
+      gitcycle
+      
+      stub_const("Gitcycle::Git", GitMock)
+      GitMock.load
+
+      request, response = json_schema_params(:branch, :get,
+        :response => { :github_issue_id => 123 }
+      )
+      gitcycle.stub(:sync).and_return(response)
+      gitcycle.stub(:pr)
+    end
+
+    it "calls Git with proper parameters" do
+      Gitcycle::Git.should_receive(:branch).ordered.
+        with("qa-name", :delete => true)
+
+      Gitcycle::Git.should_receive(:push).ordered.
+        with(":qa-name")
+      
+      Gitcycle::Git.should_receive(:checkout).ordered.
+        with("qa-name", :branch => true)
+      
+      Gitcycle::Git.should_receive(:merge_squash).ordered.
+        with("name")
+      
+      Gitcycle::Git.should_receive(:commit).ordered.
+        with("#123 title")
+      
+      Gitcycle::Git.should_receive(:push).ordered.
+        with("qa-name")
+      
+      Gitcycle::Git.should_receive(:checkout).ordered.
+        with("name")
+
+      gitcycle.ready
+    end
+  end
+end
