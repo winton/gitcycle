9e9509138a15e4e9af045f807315b86be3f2b4a8
Winton Welsh <mail@wintoni.us>
2013-12-30 21:36:23 -0800

    Starting on Git spec

---
+++ spec/gitcycle/git_spec.rb
@@ -0,0 +1,40 @@
+require File.expand_path("../../spec_helper", __FILE__)
+
+describe Gitcycle::Git do
+
+  let(:config)  { Gitcycle::Config }
+  let(:git)     { Gitcycle::Git }
+  let(:git_url) { "git@github.com:git_login/git_repo.git" }
+
+  before :each do
+    git.stub(:config_path).and_return("/")    
+    File.stub(:read).and_return("[remote \"origin\"] url = #{git_url}")
+    git.load
+  end
+
+  describe "#load" do
+
+    context "when using SSL git URL" do
+      it "assigns config properties" do
+        config.git_url.should   == git_url
+        config.git_repo.should  == "git_repo"
+        config.git_login.should == "git_login"
+      end
+    end
+
+    context "when using HTTP git URL" do
+
+      let(:git_url) { "https://github.com/git_login/git_repo.git" }
+
+      before :each do
+        File.stub(:read).and_return("[remote \"origin\"] url = #{git_url}")
+      end
+
+      it "assigns config properties" do
+        config.git_url.should   == git_url
+        config.git_repo.should  == "git_repo"
+        config.git_login.should == "git_login"
+      end
+    end
+  end
+end
