fe01d4012eefad1952bf1ce00284a395ce2f8447
Winton Welsh <mail@wintoni.us>
2014-01-06 11:51:15 -0800

    Decreasing complexity on Merge

--- lib/gitcycle/git/merge.rb
+++ lib/gitcycle/git/merge.rb
@@ -3,8 +3,7 @@ class Gitcycle < Thor
     module Merge
 
       def merge(remote, branch_name=nil)
-        remote, branch_name = params(remote, branch_name)
-        git("rebase #{remote}/#{branch_name}")
+        merge_git("rebase", remote, branch_name)
       end
 
       def merge_remote_branch(remote, repo, branch_name)
@@ -16,8 +15,14 @@ class Gitcycle < Thor
       end
 
       def merge_squash(remote, branch_name=nil)
+        merge_git("merge --squash", remote, branch_name)
+      end
+
+      private
+        
+      def merge_git(cmd, remote, branch_name)
         remote, branch_name = params(remote, branch_name)
-        git("merge --squash #{remote}/#{branch_name}")
+        git("#{cmd} #{remote}/#{branch_name}")
       end
     end
   end
