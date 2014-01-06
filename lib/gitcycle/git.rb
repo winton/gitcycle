53c1d895335cc42bfb2ff79acc0373219bedaf16
Winton Welsh <mail@wintoni.us>
2014-01-06 01:30:38 -0800

    Reducing Git.git complexity

--- lib/gitcycle/git.rb
+++ lib/gitcycle/git.rb
@@ -170,19 +170,23 @@ class Gitcycle < Thor
         log(output)
 
         if !options[:force] && errored?(output)
-          puts "Failed: git #{cmd}".red.space
-          puts log.last.gsub(/^/, "  ")
-          puts ""
-
-          begin; raise; rescue => e
-            puts e.backtrace.join("\n  ")
-          end
-
-          puts ""
-          exit 1
+          git_fail(cmd)
         end
 
         output
+      end
+
+      def git_fail(cmd)
+        puts "Failed: git #{cmd}".red.space
+        puts log.last.gsub(/^/, "  ")
+        puts ""
+
+        begin; raise; rescue => e
+          puts e.backtrace.join("\n  ")
+        end
+
+        puts ""
+        exit 1
       end
 
       def params(remote, branch_name=nil, options={})
